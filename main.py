fa830c16dabce3d99709b29361ccddff217b41e2
KCY <rrrfffrrr@naver.com>
2020-08-12 14:53:31 +0900

    Box bot token
    Box bot token to setting file

--- main.py
+++ main.py
@@ -16,11 +16,11 @@ AVAILABLE_CHANNELS = ['team-강찬영', 'general']
 MemberList.Init()
 
 class MyClient(discord.Client):
-    game = discord.Game("대홍단감자")
-    await client.change_presence(status=discord.Status.online, activity=game)
-
     async def on_message(self, message):
         ### Validation
+        game = discord.Game("Wedge Potato | [명령어") # <- ? :D 
+        await client.change_presence(status=discord.Status.online, activity=game)
+
         if message.author == self.user: # 자기 메세지 반응 안함
             return
         
@@ -35,8 +35,8 @@ class MyClient(discord.Client):
             await self.on_command(message)
             return
 
-
     async def on_command(self, message):
+        # 모든 서버 멤버
         if message.content == '모든멤버': # JARAM ONLINE의 모든 사람들을 출력함
             send_buffer = ""
             count = 0
@@ -50,6 +50,33 @@ class MyClient(discord.Client):
             await message.channel.send(embed=embed)
             return
             
+        # 감자 멤버
+        if message.content == '멤버':
+            send_buffer = "" # 캐싱
+            count=0
+
+            for id in MemberList.GetMembers():
+                count +=1
+                send_buffer += "Potato addict" + str(count) + " : " + str(self.get_user(int(id))) + "\n"
+            embed = discord.Embed(title="Potato addicts", description=send_buffer, color=0x00ff50)
+            embed.set_author(name="Those who love potato wedges")
+            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/723859914090479656/742914673137025105/jaram_photo.jpg")
+            await message.channel.send(embed = embed) # 보내기(느림)
+            return
+
+        if message.content.startswith('멤버추가'): # .startswith : !멤버추가 로 시작하는 message는 모두 받음
+            realtemp = message.content.split(" ")
+            MemberList.AddMember(int(realtemp[1]))
+            await message.channel.send('멤버 추가 완료')
+            return
+            
+        if message.content.startswith('멤버제거'):
+            realtemp = message.content.split(" ")
+            MemberList.RemoveMember(int(realtemp[1]))
+            await message.channel.send('멤버 제거 완료')
+            return
+            
+        # 사다리 타기
         if message.content[0:3] == '사다리':
             right_message = message.content.split()[1:]
             membercount = len(right_message)
@@ -84,7 +111,7 @@ class MyClient(discord.Client):
             # goal list(1이 당첨) 
             goal = ["0"] * membercount  
             goal[random.randint(0, membercount-1)] = "1"
-            await message.channel.send("　" + '      '.join(goal)) #  
+            await message.channel.send("　 " + '      '.join(goal)) #  
             # 인원출력
             
             # right_message에 인원목록 들어있음 list형식
@@ -118,43 +145,8 @@ class MyClient(discord.Client):
                     await message.channel.send(player + " 당첨")
             await message.channel.send("사다리 종료")
             return
-
-        
-
-
-        if message.content.isdigit():
-            send_buffer = "```" + str(int(message.content)**2)+ "```"
-            await message.channel.send(send_buffer)
-            return
-
-
-        if message.content == '멤버':
-            send_buffer = "" # 캐싱
-            count=0
-
-            for id in MemberList.GetMembers():
-                count +=1
-                send_buffer += "Potato addict" + str(count) + " : " + str(self.get_user(int(id))) + "\n"
-            embed = discord.Embed(title="Potato addicts", description=send_buffer, color=0x00ff50)
-            embed.set_author(name="Those who love potato wedges")
-            embed.set_thumbnail(url="https://cdn.discordapp.com/attachments/723859914090479656/742914673137025105/jaram_photo.jpg")
-            await message.channel.send(embed = embed) # 보내기(느림)
-            return
-
-
-        if message.content.startswith('멤버추가'): # .startswith : !멤버추가 로 시작하는 message는 모두 받음
-            realtemp = message.content.split(" ")
-            MemberList.AddMember(int(realtemp[1]))
-            await message.channel.send('멤버 추가 완료')
-            return
             
-        if message.content.startswith('멤버제거'):
-            realtemp = message.content.split(" ")
-            MemberList.RemoveMember(int(realtemp[1]))
-            await message.channel.send('멤버 제거 완료')
-            return
-
-
+        # 뽑기
         if message.content.startswith('뽑기'): # 뽑기 작동해여
             i=0
             temp = message.content.split(" ")
@@ -185,8 +177,8 @@ class MyClient(discord.Client):
             embed.set_author(name="뽑기")
             await message.channel.send(embed = embed) # 보내기(느림)
             return
-
-
+            
+        # 369 게임
         if message.content == "369 준비":
             await Game369.Ready(self, message)
             return
@@ -210,8 +202,14 @@ class MyClient(discord.Client):
             \'짝\' 말고도  \"짝\", \"작\", \"쨕\", \"쟉\", \"쨖\", \"쟊\", \"짞\", \"쟊\"이 가능합니다.\
             처음 제한시간은 15초지만 점점 빨라집니다. 그래서 후에는 2초마다 타이핑 하셔야 합니다.")
             return
-        
-
+            
+        # 제곱 기능
+        if message.content.isdigit():
+            send_buffer = "```" + str(int(message.content)**2)+ "```"
+            await message.channel.send(send_buffer)
+            return
+            
+        # 명령어 목록
         if message.content.startswith("명령어"):
             embed = discord.Embed(
                 title = 'Command List',
@@ -246,5 +244,8 @@ class MyClient(discord.Client):
 # 2. 실행 (터미널 누르고 ctrl + c 누른 뒤 -> python main.py)
 
 # self.
+f = open("Setting/token", 'r')
+token = f.read().strip()
+f.close()
 client = MyClient() # 생성
-client.run('NzQyNTY5MzM4MTk5OTk4NDc0.XzIBlg.DbAHj1SYzSElj9mKpx-nTQVkh_c') # 토큰을 이용하여 활성화?
+client.run(token) # 토큰을 이용하여 활성화
